93200e53595e210c1aa1026dbd0a90308221c6a7
BruceTraining <b@btauthors.com>
2026-01-28 15:28:40 +0100

    Add files via upload

---
+++ stock_mcp_http/__init__.py
@@ -0,0 +1,8 @@
+"""
+Stock MCP HTTP Server Package
+
+A Model Context Protocol server providing stock price information
+via HTTP streaming transport for ChatGPT Apps compatibility.
+"""
+
+__version__ = "0.1.0"
